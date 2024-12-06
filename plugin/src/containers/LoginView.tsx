09d22d1701ba4854cc88169dbfb41aea3691580b
Emil Widlund <hello@emilwidlund.com>
2024-12-06 11:10:25 +0100

    add small link to polar

--- plugin/src/containers/LoginView.tsx
+++ plugin/src/containers/LoginView.tsx
@@ -74,15 +74,20 @@ export const LoginView = ({ onSuccess }: LoginViewProps) => {
   };
 
   return (
-    <div className="flex flex-grow flex-col items-center justify-center gap-8 p-4 w-full">
-      <LogoIcon size={50} />
-      <h1 className="text-lg font-medium text-center text-pretty px-4">
-        Digital Products & Payments <span className="text-neutral-500">made easy</span>
-      </h1>
+    <div className="flex flex-grow flex-col justify-between gap-8 px-4 py-8 w-full">
+      <div className="flex flex-col flex-grow items-center justify-center gap-8">
+        <LogoIcon size={50} />
+        <h1 className="text-lg font-medium text-center text-pretty px-4">
+          Digital Products & Payments <span className="text-neutral-500">made easy</span>
+        </h1>
         <Button className="w-fit px-6 rounded-full" onClick={login}>
           <span>Get Started</span>
           <KeyboardArrowRight className="w-6 h-6" />
         </Button>
+      </div>
+        <div className="text-xs flex flex-col items-center hover:text-neutral-500 transition-colors">
+          <a href="https://polar.sh" target="_blank" rel="noreferrer">polar.sh</a>
+        </div>
     </div>
   );
 };
